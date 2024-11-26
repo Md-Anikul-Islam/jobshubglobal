17ca23bd255b57c040a7cc2ea061093769bd336c
Md-Anikul-Islam <anikul.islam01@northsouth.edu>
2024-11-26 12:57:44 +0600

    update

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -41,10 +41,10 @@
                 <td>{{ ++$i }}</td>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
-                <td>
+                <td >
                     @if(!empty($user->getRoleNames()))
                         @foreach($user->getRoleNames() as $v)
-                            <label class="badge badge-success">{{ $v }}</label>
+                            <b class="badge badge-success" style="color:#000;">{{ $v }}</b>
                         @endforeach
                     @endif
                 </td>
