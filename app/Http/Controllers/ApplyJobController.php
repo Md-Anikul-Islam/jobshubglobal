a8f6c1908cb0b49ae9a7395cf9ae385a5462a4bc
Md-Anikul-Islam-Binju <anikul.islam.binju@gmail.com>
2025-09-02 14:40:30 +0600

    update

--- app/Http/Controllers/ApplyJobController.php
+++ app/Http/Controllers/ApplyJobController.php
@@ -40,18 +40,24 @@ class ApplyJobController extends Controller
         return redirect()->back();
     }
 
-    function sendSms($mobile, $sms) {
+    function sendSms($mobile, $sms)
+    {
         $url = 'https://bulksms.teletalk.com.bd/jlinktbls.php';
 
         $user = env('SMS_API_USERNAME');
-        $pass = env('SMS_API_PASSWORD');
-        $userId = '11331';
-        $encrKey = '@***';
+        $apiPassword = env('SMS_API_PASSWORD');
+        $userId = env('SMS_API_USERID', '11331'); // Move to .env for flexibility
+        $encrKey = env('SMS_API_ENCR_KEY', '@***'); // Move to .env
+        $cid = env('SMS_API_CID', '8801552146406'); // Move to .env
+
+        // Hash password using MD5
+        $pass = md5($apiPassword);
 
         // Generate 16-digit p_key
         $p_key = str_pad(strval(random_int(0, 9999999999999999)), 16, '0', STR_PAD_LEFT);
 
-        $a_key = md5($userId . $p_key . $encrKey);
+        // Correct a_key generation
+        $a_key = md5(($userId + $p_key) . $encrKey);
 
         $data = [
             'op' => 'SMS',
@@ -64,7 +70,7 @@ class ApplyJobController extends Controller
             'validity' => '1440',
             'a_key' => $a_key,
             'p_key' => $p_key,
-            'cid' => '8801552146406' // <-- your Bill MSISDN
+            'cid' => $cid,
         ];
 
         $payload = json_encode($data);
@@ -77,79 +83,22 @@ class ApplyJobController extends Controller
             'Content-Type: application/json',
             'Content-Length: ' . strlen($payload)
         ]);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // for debug only
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // for debug only
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // For testing only
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // For testing only
 
         $response = curl_exec($ch);
         if ($response === false) {
-            dd(curl_error($ch));
+            \Log::error('Teletalk SMS Error: ' . curl_error($ch));
         }
         curl_close($ch);
 
-        dd($response); // <-- inspect API response
+        \Log::info('Teletalk SMS Response: ' . $response);
 
         return $response;
     }
 
 
 
-
-
-
-
-
-
-
-
-//    private function sendSms($mobile, $sms)
-//    {
-//        $data = [
-//            'op' => 'SMS',
-//            'chunk' => 'S',
-//            'user' => env('SMS_API_USERNAME'),
-//            'pass' => env('SMS_API_PASSWORD'),
-//            'smsclass' => 'GENERAL',
-//            'mobile' => $mobile,
-//            'sms' => $sms,
-//            'charset' => 'UTF-8',
-//            'validity' => '1440',
-//            'a_key' => '0f10d53d80db812c581745a1834054e8',
-//            'p_key' => '1595157663255938',
-//            'cid' => uniqid(),
-//        ];
-//
-//        $ch = curl_init('https://bulksms.teletalk.com.bd/jlinktbls.php');
-//        curl_setopt($ch, CURLOPT_POST, true);
-//        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-//        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//
-//        $response = curl_exec($ch);
-//        curl_close($ch);
-//
-//        return $response;
-//    }
-
-
-
-//    private function sendSms($mobile, $sms)
-//    {
-//        $url = 'https://bulksms.teletalk.com.bd/jlinktbls.php?' . http_build_query([
-//                'op'      => 'SMS',
-//                'user'    => env('SMS_API_USERNAME', 'iTuring'),
-//                'pass'    => env('SMS_API_PASSWORD', ''),
-//                'mobile'  => $mobile,
-//                'charset' => 'UTF-8',
-//                'sms'     => $sms
-//            ]);
-//
-//        $ch = curl_init($url);
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//        $response = curl_exec($ch);
-//        curl_close($ch);
-//        return $response;
-//    }
 
 
 //    public function applyJobList()
